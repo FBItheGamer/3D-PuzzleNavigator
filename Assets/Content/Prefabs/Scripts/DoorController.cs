19f248334b7684eed9e235818e0be0a74a0fc3b7
FBItheGamer <158209074+FBItheGamer@users.noreply.github.com>
2025-04-27 19:19:07 -0400

    Renamed Terrain to TerrainOld

---
+++ Assets/Content/Prefabs/Scripts/DoorController.cs
@@ -0,0 +1,68 @@
+<<<<<<< HEAD:Assets/Content/Scripts/DoorController.cs
+using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public string KeycardID;
+    private bool isOpen = false;
+    private Animator animator;
+    //private MySingletonClass mySingletonClass;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("isOpen", false);
+    }
+
+    public void OpenAndClose()
+    {
+        if (isOpen == false)
+        {
+            isOpen = true;
+            animator.SetBool("isOpen", true);
+            Debug.Log("Door Opened");
+        }
+        else if (isOpen == true)
+        {
+            isOpen = false;
+            animator.SetBool("isOpen", false);
+            Debug.Log("Door Closed");
+        }
+    }
+}
+=======
+using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public string KeycardID;
+    private bool isOpen = false;
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("isOpen", false);
+    }
+
+    public void OpenAndClose()
+    {
+        if (isOpen == false)
+        {
+            isOpen = true;
+            animator.SetBool("isOpen", true);
+            Debug.Log("Door Opened");
+        }
+        else if (isOpen == true)
+        {
+            isOpen = false;
+            animator.SetBool("isOpen", false);
+            Debug.Log("Door Closed");
+        }
+    }
+}
+>>>>>>> FBX_Tests:Assets/Content/Prefabs/Scripts/DoorController.cs
